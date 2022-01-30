e836def8d57f4aa9677ccbd209cd40a8d9a97810
Chintan Suthar <75067919+chintansuthar72@users.noreply.github.com>
2022-01-30 17:07:04 +0530

    added JWT authentication - Checkpoint 4

---
+++ README.md
@@ -0,0 +1,3 @@
+### See <i><b>screenshots</b></i> folder for website's look...
+
+### https://blog-app-72.herokuapp.com
--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');   
 const Blog = require('./models/blogModel');
+const User = require('./models/userModel');
+const validate = require('./validation');
+const jwt = require('jsonwebtoken');
+const verify = require('./verifyToken')
 const fs = require('fs');
 const multer = require('multer');
 const path = require('path');
@@ -34,62 +39,120 @@ const upload = multer({
 mongoose.connect(process.env.DATABASE_URL)
     .then((result)=>{
         console.log("connected to database...");
-        app.listen(7000,'localhost',()=>{
-            console.log("listening on port 7000...");
+        app.listen(process.env.PORT,'localhost',()=>{
+            console.log("listening on port " + process.env.PORT);
         })
     }) .catch((error)=>{
         console.log(error);
     });
-
-app.use((req,res,next) => {
-    res.setHeader("Access-Control-Allow-Origin","*");
-    next();
-})
 
 app.set('view engine','ejs');
 app.set('views','views')
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/user/signin', (req,res)=>{
+    res.render('signin');
+});
+
+app.post('/user/signin', async (req,res)=>{ 
+    const validation = validate.signinValidation(req.body);
+    if(validation.error){
+        res.status(400).send(validation.error.details[0].message)
+    } else  {
+        const checkForEmail = await User.findOne({email : req.body.email});
+        if(checkForEmail){
+            res.status(400).send("email already exists")
+        } else {
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(req.body.password, salt);
+            const user = new User({
+                username : req.body.username,
+                email : req.body.email,
+                password : hashedPassword 
+            });
+            user.save()
+            .then(result => res.render('login',{message:"You can now login!"}))
+            .catch(err => res.status(400).send(err))
+        }
+    }  
+});
+
+app.get('/user/login',(req,res) => {
+    res.render('login',{message:""});
+})
+
+app.post('/user/login', async (req,res)=>{
+    const validation = validate.loginvalidation(req.body)
+    if(validation.error){
+        res.status(400).send(validation.error.details[0].message)
+    } else{
+        const user = await User.findOne({email : req.body.email});
+        if(!user) {
+            res.status(400).send("User does not exists!")
+        } else {
+            const valid = await bcrypt.compare(req.body.password,user.password);
+            if(!valid){
+                res.status(400).send("Invalid password")
+            } else {
+                const token = jwt.sign({_id : user._id}, process.env.TOKEN_SECRET);
+                // res.header('auth-token', token)
+                res.redirect(`/blogs?token=${token}`)
+            }
+        }
+    }
+});
+
 app.get('/', (req,res) => {
-    res.redirect('/blogs');
+    res.redirect('/user/login');
 });
 
 app.get('/create', (req,res)=>{
-    res.render('create');
+    const token = req.query.token;
+    res.render('create',{token});
 })
 
-app.get('/blogs', (req,res) => {
-    Blog.find().sort({createdAt : -1}).then((result) => {
-        res.render('index',{blogs : result}); 
+app.get('/blogs',verify, async (req,res) => {
+    const id = req.user._id;
+    const user = await User.findById(id);
+    const email = user.email;
+    const token = req.query.token;
+    Blog.find({email}).sort({createdAt : -1}).then((result) => {
+        res.render('index',{blogs : result,token : token, username : user.username}); 
     }).catch(err => res.send(err));
 })
 
-app.post('/blogs', upload.single('image'), (req,res)=>{
+app.post('/blogs', verify, upload.single('image'), async (req,res)=>{
+    const token = req.query.token;
+    const id = req.user._id;
+    const user = await User.findById(id);
     const blog = new Blog({
         title : req.body.title,
         body : req.body.body,
+        email : user.email
     })
     if(req.file){
         blog.image = req.file.filename
     }
     blog.save().then(result => {
-        res.redirect('/blogs');
+        res.redirect(`/blogs?token=${token}`);
     }).catch(err => res.send(err));
 })
 
 app.get('/blogs/:id', (req,res)=>{
     const id = req.params.id;
+    const token = req.query.token;
     Blog.findById(id).then((result)=>{
-        res.render('details',{blog:result});
+        res.render('details',{blog:result,token:token});
     }).catch(err => res.send(err));
 })
 
 app.delete('/blogs/:id', (req, res) => {
+    const token = req.query.token;
     const id = req.params.id;
     Blog.findByIdAndDelete(id)
     .then(result => {
-        res.json({ redirect: '/blogs' });
+        res.json({ redirect: `/blogs?token=${token}` });
     })
     .catch(err => {
         res.send(err);
@@ -97,16 +160,18 @@ app.delete('/blogs/:id', (req, res) => {
 });
 
 app.get('/blogs/update/:id',(req,res)=> {
+    const token = req.query.token;
     const id = req.params.id;
     Blog.findById(id).then(result => {
-        res.render('update',{blog:result});
+        res.render('update',{blog:result,token});
     }).catch(err => res.send(err));
 });
 
 app.post('/blogs/update/:id', (req,res) => {
+    const token = req.query.token;
     const id = req.params.id;
     Blog.findByIdAndUpdate(id,req.body).then(result => {
-        res.redirect(`/blogs/${id}`)
+        res.redirect(`/blogs/${id}?token=${token}`)
     }).catch(err => res.send(err));
 })
 
